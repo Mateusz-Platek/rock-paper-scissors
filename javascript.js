d01da80cf09813ecf0e5de7103d7823d1602aec9
Mateusz-Platek <m.platek1705@gmail.com>
2022-08-11 13:34:05 +0200

    Create two functions. First asks player to choose atribute. Second decides who won the round

--- javascript.js
+++ javascript.js
@@ -8,3 +8,31 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function getPlayerChoice() {
+    let val = prompt("What is your choice?");
+    return val.toLowerCase();
+}
+
+function round(computer, player) {
+    let compVal = computer();
+    let playerVal = player();
+    
+    if(compVal == playerVal) {
+        return `Draw! You both chose ${compVal}`;
+    } else {
+        if(compVal == "rock" && player == "paper") {
+            return `You Win! ${playerVal} beats ${compVal}`;
+        } else if(compVal == "rock" && player == "scissors") {
+            return `You Lose! ${compVal} beats ${playerVal}`;
+        } else if(compVal == "paper" && player == "scissors") {
+            return `You Win! ${playerVal} beats ${compVal}`;
+        } else if(compVal == "paper" && player == "rock") {
+            return `You Lose! ${compVal} beats ${playerVal}`;
+        } else if(compVal == "scissors" && player == "rock") {
+            return `You Win! ${playerVal} beats ${compVal}`;
+        } else {
+            return `You Lose! ${compVal} beats ${playerVal}`;
+        }
+    }
+}
